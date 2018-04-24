2bbf329713633919db5c5b9d3948be167e6ec529
Milan Patel <mgpatel2@asu.edu>
2018-04-23 20:22:03 -0700

    Compiled all data (excluding material properties) in conditions.py and updated simulation.py and main.py accordingly. Seasonal data and simulation has been implemented.

--- Work/conditions.py
+++ Work/conditions.py
@@ -1,3 +1,114 @@
 # conditions.py
 # This file contains all of the boundary and initial conditions that will be
 # used to define the various problems being simulated.
+
+import numpy as np
+
+# Constants:
+# Note: All values documented below and used within the simulations are defined
+#       in terms of SI base units.
+#
+#           Distance    = Meters (m)
+#           Time        = Seconds (s)
+#           Temperature = Kelvin (K)
+
+#====================#
+# Default Parameters #
+#====================#
+time = 48 * 3600    # 48-Hour Simulation Period
+
+# Discretization
+dx = 0.1
+dy = 0.1
+dt = 2
+
+# Plotting Steps
+steps = 20
+
+#=====================#
+# Material Properties #
+#=====================#
+
+# Materials List:
+materialList = \
+[
+    'air',
+    'iron'
+]
+
+# Enumeration:
+class material:
+    air, iron = range(len(materialList))
+
+# Initialize Data Containers:
+kappa = np.zeros(len(materialList))
+c_heat = np.zeros(len(materialList))
+rho = np.zeros(len(materialList))
+
+# Thermal Conductivity:
+kappa[material.air] = 2.624
+kappa[1] = 237
+
+# Specific Heat:
+c_heat[material.air] = 1000
+c_heat[1] = 900
+
+# Density:
+rho[material.air] = 1.204
+rho[1] = 2700
+
+#======================#
+# Initial and Boundary #
+#      Conditions      #
+#======================#
+
+# Initial Interior Temperature:
+T0 = 75 # Room Temperature (degrees F)
+
+# The average high and low temperatures are taken from a collection of data sta-
+# tistically averaged from 1981 until 2010 in Phoenix, AZ. In particular, the
+# summer temperatures are taken from an average over June to September while the
+# winter temperatures are taken from an average over November to February based
+# on the seasons in the Northern Hemisphere.
+summer_high = 103.5 # Summer High Temperature (degrees F)
+summer_low = 79.25  # Summer Low Temperature (degrees F)
+
+winter_high = 71    # Winter High Temperature (degrees F)
+winter_low = 48     # Winter Low Temperature (degrees F)
+
+# Convert from Fahrenheit to Kelvin:
+def FahrenheitToKelvin(temperatureF):
+    temperatureK = (5/9) * (temperatureF + 459.67)
+    return temperatureK
+
+# Initial Interior Temperature:
+T0 = FahrenheitToKelvin(T0)
+
+# Seasonal Temperature Extremes:
+summer_high = FahrenheitToKelvin(summer_high)
+summer_low = FahrenheitToKelvin(summer_low)
+
+winter_high = FahrenheitToKelvin(winter_high)
+winter_low = FahrenheitToKelvin(winter_low)
+
+# Temperature Averages:
+summer_average = (summer_high + summer_low) / 2
+winter_average = (winter_high + winter_low) / 2
+
+average = np.array([summer_average, winter_average])
+
+# Temperature Variations/Amplitudes:
+summer_amplitude = (summer_high - summer_low) / 2
+winter_amplitude = (winter_high - winter_low) / 2
+
+amplitude = np.array([summer_amplitude, winter_amplitude])
+
+# Temperature Offset:
+# The high temperatures occur near 5 pm and the low temperatures occur near 3
+# am; however for convenience, a 24-hour period is used, setting the high temp-
+# erature to occur at 4 pm while the low temperature is set to occur at 4 am.
+delta = (2/3) * np.pi
+
+# Boundary conditions:
+def boundary(season, t):
+    return average[season] + amplitude[season] * np.cos((2*np.pi*t)/(24*3600) + delta)
--- Work/main.py
+++ Work/main.py
@@ -2,9 +2,25 @@
 # All of the analysis is functionally called upon within this file, compiling
 # all of the results into a neat and readable manner.
 
+import numpy as np
+
+# Import associated files.
 import simulation as sim
 import plotting as plot
 
-#T_plot, (dx, dt, step) = sim.CrankNicolson_T(L_rod = 10, t_max = 7200, Dx = 0.1)
-T_plot, (dx, dt, step) = sim.CrankNicolson_1D(50, 172800, 0.1, 2, None, 1)
+# Materials List:
+materialList = \
+[
+    'air',
+    'iron'
+]
+
+# Enumerations:
+class material:
+    air, iron = range(len(materialList))
+
+class season:
+    summer, winter = range(2)
+
+T_plot, (dx, dt, step) = sim.CrankNicolson_1D(season.summer, material.iron, 1, 50)
 plot.plot_T(T_plot, dx, dt, step)
